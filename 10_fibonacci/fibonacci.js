9025f5b9bde5fe6f018dbd85343e411a86d1caae
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-09-12 18:57:00 +0200

    feat: add solution to problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    if (index == 1 || index == 2) return 1;
 
+    let firstPreceding = 1;
+    let secondPreceding = 1;
+    let total = 0;
+
+    for(let i = 2; i < index; i++) {
+        total = firstPreceding + secondPreceding;
+        firstPreceding = secondPreceding;
+        secondPreceding = total;
+    }
+    return total;
 };
 
 // Do not edit below this line
