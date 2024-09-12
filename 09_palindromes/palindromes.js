38c307e03ce84b2179a608a0feee7402d0c210fe
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-09-12 18:29:56 +0200

    feat: add solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strLength = str.replace(/[^\w]/gi, '').length;
+    const isEven = strLength % 2 === 0 ? true : false;
+    const middle = isEven ? strLength / 2 : Math.ceil(strLength / 2);
 
+    let firstHalf = "";
+    let secondHalf = "";
+
+    if(isEven) {
+        firstHalf = str.toLowerCase().replace(/[^\w]/gi, '').substr(0,middle);
+        secondHalf = str.toLowerCase().replace(/[^\w]/gi, '').substr(middle, strLength).split('').reverse().join('');
+    } else {
+        firstHalf = str.toLowerCase().replace(/[^\w]/gi, '').substr(0,middle);
+        secondHalf = str.toLowerCase().replace(/[^\w]/gi, '').substr(middle-1, strLength).split('').reverse().join('');
+    }
+
+    return firstHalf === secondHalf ? true : false;
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
