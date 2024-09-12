d3ee829f5bfe5a96c4389dcc7369e40856c27e08
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-09-12 17:39:36 +0200

    feat: add solution for exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, currentItem) => {
+    return add(total, currentItem);
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponential) {
+	return Math.pow(base, exponential);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for(let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
