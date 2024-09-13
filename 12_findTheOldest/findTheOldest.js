d4a2b0706a9b4ce4d32aad0353cc8ebbb56c686a
Alan Blanco <66321582+detrett@users.noreply.github.com>
2024-09-13 12:27:39 +0200

    feat: add solution to exercises 11 & 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((accumulator, person) => {
+        const thisYear = new Date().getFullYear();
+        const personYearOfDeath = person.yearOfDeath || thisYear;
+        const accumulatorYearOfDeath = accumulator.yearOfDeath || thisYear;
 
+        const personAge = personYearOfDeath - person.yearOfBirth;
+        const accumulatorAge = accumulatorYearOfDeath - accumulator.yearOfBirth;
+        return personAge > accumulatorAge ? person : accumulator;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
